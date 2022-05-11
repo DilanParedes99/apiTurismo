714a3249bf27fa7d68ffe12006f55486b4040088
Brian Dilan Garcia Paredes <dilanparedes2011@hotmail.com>
2022-05-10 23:38:53 -0500

    se agrego la BD que se llama turismo

--- turismo.sql
+++ turismo.sql
@@ -1,57 +1,237 @@
-BEGIN TRANSACTION
-CREATE TABLE IF NOT EXISTS "usuarios" (
-	"idUsuario"	INTEGER UNIQUE,
-	"nombreUsuario"	TEXT,
-	"primerApellido"	TEXT,
-	"segundoApellido"	TEXT,
-	"correo"	TEXT UNIQUE,
-	"tipo"	TEXT,
-	PRIMARY KEY("idUsuario" AUTOINCREMENT)
-);
-CREATE TABLE IF NOT EXISTS "empresas" (
-	"idEmpresa"	INTEGER UNIQUE,
-	"razonSocial"	TEXT,
-	"rfc"	TEXT UNIQUE,
-	PRIMARY KEY("idEmpresa" AUTOINCREMENT)
-);
-CREATE TABLE IF NOT EXISTS "usuariosEmpresa" (
-	"idUsuario"	INTEGER,
-	"idEmpresa"	INTEGER,
-	PRIMARY KEY("idEmpresa","idUsuario")
-);
-CREATE TABLE IF NOT EXISTS "servicios" (
-	"idServicio"	INTEGER,
-	"idEmpresa"	INTEGER,
-	"estado"	INTEGER,
-	"nombre"	TEXT,
-	"descripcion"	TEXT,
-	"costoPersona"	REAL,
-	"precioPersona"	REAL,
-	"fechaInicio"	INTEGER,
-	"fechaFinal"	TEXT,
-	"disponibilidad"	INTEGER,
-	PRIMARY KEY("idServicio" AUTOINCREMENT)
-);
-CREATE TABLE IF NOT EXISTS "solicitudesReservacion" (
-	"idSolicitud"	INTEGER,
-	"idServicio"	INTEGER,
-	"correoCliente"	INTEGER,
-	PRIMARY KEY("idSolicitud" AUTOINCREMENT)
-);
-CREATE TABLE IF NOT EXISTS "reservaciones" (
-	"idReservacion"	INTEGER,
-	"idSolicitd"	INTEGER UNIQUE,
-	"ticket"	TEXT UNIQUE,
-	PRIMARY KEY("idReservacion" AUTOINCREMENT)
-);
-CREATE TABLE IF NOT EXISTS "productos" (
-	"idProducto"	INTEGER,
-	"idEmpresa"	INTEGER,
-	"nombre"	TEXT,
-	"descripcion"	TEXT,
-	"costo"	REAL,
-	"precio"	REAL,
-	"stock"	INTEGER,
-	PRIMARY KEY("idProducto" AUTOINCREMENT)
-);
-COMMIT;
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: turismo-back
+-- ------------------------------------------------------
+-- Server version	5.7.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `empresas`
+--
+
+DROP TABLE IF EXISTS `empresas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empresas` (
+  `idEmpresa` int(10) NOT NULL AUTO_INCREMENT,
+  `razonSocial` text,
+  `rfc` text,
+  PRIMARY KEY (`idEmpresa`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empresas`
+--
+
+LOCK TABLES `empresas` WRITE;
+/*!40000 ALTER TABLE `empresas` DISABLE KEYS */;
+INSERT INTO `empresas` VALUES (1,'MEXICANA','MEDE125423'),(2,'MEXICANA','MEDE125423'),(3,'MEXICANA','MEDE125423'),(4,'MEXICANA','MEDE125423');
+/*!40000 ALTER TABLE `empresas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `idProducto` int(10) NOT NULL AUTO_INCREMENT,
+  `idEmpresa` int(10) DEFAULT NULL,
+  `nombre` text,
+  `descripcion` text,
+  `costo` text,
+  `precio` text,
+  `stock` text,
+  PRIMARY KEY (`idProducto`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (1,12,'Monarca','Mariposa','234','123','12'),(2,12,'Monarca','Mariposa','234','123','12'),(3,12,'Monarca','Mariposa','234','123','12'),(4,12,'Monarca','Mariposa','234','123','12');
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservaciones`
+--
+
+DROP TABLE IF EXISTS `reservaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservaciones` (
+  `idReservacion` bigint(20) NOT NULL AUTO_INCREMENT,
+  `idSolicitd` bigint(20) NOT NULL,
+  `ticket` text,
+  PRIMARY KEY (`idReservacion`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservaciones`
+--
+
+LOCK TABLES `reservaciones` WRITE;
+/*!40000 ALTER TABLE `reservaciones` DISABLE KEYS */;
+INSERT INTO `reservaciones` VALUES (1,12,'Ticket_No12'),(2,12,'Ticket_No12'),(3,12,'Ticket_No12');
+/*!40000 ALTER TABLE `reservaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicios`
+--
+
+DROP TABLE IF EXISTS `servicios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servicios` (
+  `idServicio` text,
+  `idEmpresa` text,
+  `estado` text,
+  `nombre` text,
+  `descripcion` text,
+  `costoPersona` text,
+  `precioPersona` text,
+  `fechaInicio` text,
+  `fechaFinal` text,
+  `disponibilidad` text
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicios`
+--
+
+LOCK TABLES `servicios` WRITE;
+/*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
+INSERT INTO `servicios` VALUES ('idServicio','idEmpresa','estado','nombre','descripcion','costoPersona','precioPersona','fechaInicio','fechaFinal','disponibilidad');
+/*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `solicitudesreservacion`
+--
+
+DROP TABLE IF EXISTS `solicitudesreservacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `solicitudesreservacion` (
+  `idSolicitud` int(10) NOT NULL AUTO_INCREMENT,
+  `idServicio` int(10) DEFAULT NULL,
+  `correoCliente` int(10) DEFAULT NULL,
+  PRIMARY KEY (`idSolicitud`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solicitudesreservacion`
+--
+
+LOCK TABLES `solicitudesreservacion` WRITE;
+/*!40000 ALTER TABLE `solicitudesreservacion` DISABLE KEYS */;
+INSERT INTO `solicitudesreservacion` VALUES (1,12,32),(2,12,32),(3,12,32),(4,12,32);
+/*!40000 ALTER TABLE `solicitudesreservacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sqlite_sequence`
+--
+
+DROP TABLE IF EXISTS `sqlite_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sqlite_sequence` (
+  `name` text,
+  `seq` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sqlite_sequence`
+--
+
+LOCK TABLES `sqlite_sequence` WRITE;
+/*!40000 ALTER TABLE `sqlite_sequence` DISABLE KEYS */;
+INSERT INTO `sqlite_sequence` VALUES ('servicios',0),('solicitudesReservacion',0),('reservaciones',0),('productos',0),('usuarios',1);
+/*!40000 ALTER TABLE `sqlite_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombreUsuario` text,
+  `primerApellido` text,
+  `segundoApellido` text,
+  `correo` varchar(50) DEFAULT NULL,
+  `tipo` text,
+  `contrasena` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Brian','Garcia','Paredes','usuario@gmail.com','admin','12345'),(2,'Dilan','Garcia','Paredes','usuario1@gmail.com','admin','12345');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuariosempresa`
+--
+
+DROP TABLE IF EXISTS `usuariosempresa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuariosempresa` (
+  `idUsuario` text,
+  `idEmpresa` text
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuariosempresa`
+--
+
+LOCK TABLES `usuariosempresa` WRITE;
+/*!40000 ALTER TABLE `usuariosempresa` DISABLE KEYS */;
+INSERT INTO `usuariosempresa` VALUES ('idUsuario','idEmpresa');
+/*!40000 ALTER TABLE `usuariosempresa` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-10 23:32:20
